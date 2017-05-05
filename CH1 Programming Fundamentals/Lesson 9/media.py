d035a9a4194fcbe6a6ed480b5cdd070a06013f48
Bruno <bdiaz056@fiu.edu>
2017-05-05 05:49:52 -0400

    Lesson 9

---
+++ CH1 Programming Fundamentals/Lesson 9/media.py
@@ -0,0 +1,21 @@
+import webbrowser
+from PIL import Image
+
+class Movie():
+    def __init__(self, title, storyline, poster_image,
+                 youtube_trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = youtube_trailer
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+    def show_poster(self):
+        image = Image.open(self.poster_image_url)
+        image.show()
+
+    def description(self):
+        desc = "Title: %s\nStoryline: %s" % (self.title, self.storyline)
+        return desc
